b4926a3e1faad348f42e364405d8ed3a49db1dc1
manuelmeister <news.manuelsworld@gmail.com>
2015-05-14 00:16:00 +0200

    Added model

--- thepost/Controller/Controller.php
+++ thepost/Controller/Controller.php
@@ -16,19 +16,19 @@ class Controller {
     /**
      * @var Model
      */
-    public $db;
+    public $model;
     public $current;
 
     function __construct()
     {
-        $this->db = new Model();
+        $this->model = new Model();
 
         $request = new RequestHandler($_SERVER['REQUEST_URI']);
         $controller = $request->getController();
         $method = $request->getMethod();
         $param = $request->getParam();
 
-        $this->current = new $controller($this->db);
+        $this->current = new $controller($this->model);
         $this->current->$method($param);
     }
 
