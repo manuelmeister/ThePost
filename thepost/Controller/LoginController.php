838b861bc59e5feeec528063c4c27b3a792f9a14
manuelmeister <news.manuelsworld@gmail.com>
2015-05-14 12:51:37 +0200

    Added empty LoginController

---
+++ thepost/Controller/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manuelmeister
+ * Date: 14.05.15
+ * Time: 12:49
+ */
+
+namespace ThePost\Controller;
+
+
+class LoginController extends DefaultController {
+
+    public function login(){
+        //TODO implement login
+    }
+
+    public function submit(){
+        //TODO implement $_POST submit
+    }
+}
