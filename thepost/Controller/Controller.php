23d26a0996b96c99f789685c53ecf42850baa364
manuelmeister <news.manuelsworld@gmail.com>
2015-05-20 10:47:28 +0200

    Added vars, PHPDocs comment Split run method into create_controller, set_vars

--- thepost/Controller/Controller.php
+++ thepost/Controller/Controller.php
@@ -9,28 +9,92 @@
 namespace ThePost\Controller;
 
 
+use ThePost\Model\Entity\User;
 use ThePost\Model\Model;
 
+/**
+ * Class Controller
+ * @package ThePost\Controller
+ */
 class Controller {
 
     /**
      * @var Model
      */
-    public $model;
-    public $current;
+    private $model;
 
+    /**
+     * @var RequestHandler
+     */
+    private $request;
+
+    /**
+     * @var DefaultController
+     */
+    private $controller;
+
+    /**
+     * @var User
+     */
+    private $user;
+
+    /**
+     *
+     */
     function __construct()
     {
         $this->model = new Model();
 
-        $request = new RequestHandler($_SERVER['REQUEST_URI']);
-        $controller = $request->getController();
-        $method = $request->getMethod();
-        $param = $request->getParam();
+        session_start();
 
-        //TODO Add authentication here
-        $this->current = new $controller($this->model);
-        $this->current->$method($param);
+        $this->create_controller();
+        $this->session_management();
+        $this->init_vars();
+
+        echo $this->controller->view->render();
+    }
+
+    /**
+     *
+     */
+    private function session_management(){
+
+        if(isset($_SESSION['user_id'])){
+
+            $stmt = $this->model->pdo->prepare('SELECT * FROM `User` WHERE id =:id;');
+            $stmt->bindParam(':id',$_SESSION['user_id']);
+            $stmt->execute();
+            $this->user = $stmt->fetchObject('ThePost\Model\Entity\User');
+
+            $this->user->setLogin(true);
+        }else{
+
+            $this->user = new User();
+
+        }
+
+    }
+
+    /**
+     *
+     */
+    private function create_controller(){
+        $this->request = new RequestHandler($_SERVER['REQUEST_URI']);
+
+        $controller = $this->request->getController();
+        $method = $this->request->getMethod();
+        $param = $this->request->getParam();
+
+        $this->controller = new $controller($this->model);
+        $this->controller->$method($param);
+    }
+
+    /**
+     *
+     */
+    private function init_vars(){
+        $this->controller->set_authentication($this->user);
+        $this->controller->set_vars();
     }
 
 }
