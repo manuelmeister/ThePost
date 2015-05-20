387ccacc5f743733e77de195725987ec436b913d
manuelmeister <news.manuelsworld@gmail.com>
2015-05-20 10:36:04 +0200

    Added render_vars Moved render to this class

--- thepost/View/View.php
+++ thepost/View/View.php
@@ -8,24 +8,33 @@
 
 namespace ThePost\View;
 
+
 /**
  * Class View
  * @package ThePost\View
  */
-class View {
+class View
+{
 
     /**
      * @var \Twig_Loader_Filesystem
      */
     private $loader;
+
     /**
      * @var \Twig_Environment
      */
     protected $twig;
+
     /**
      * @var \Twig_TemplateInterface
      */
     protected $template;
+
+    /**
+     * @var array
+     */
+    protected $render_vars = array();
 
     /**
      *
@@ -33,14 +42,43 @@ class View {
     function __construct()
     {
         \Twig_Autoloader::register();
-        $this->loader = new \Twig_Loader_Filesystem(__DIR__.'/Templates');
+        $this->loader = new \Twig_Loader_Filesystem(__DIR__ . '/Templates');
         $this->twig = new \Twig_Environment($this->loader);
+    }
+
+    /**
+     * @param $options
+     * @param $authentication
+     */
+    public function set_render_vars($options, $authentication)
+    {
+        $this->render_vars['options'] = $options;
+        $this->render_vars['authentication'] = $authentication;
+    }
+
+    /**
+     * @param $arr
+     */
+    public function add_render_vars($arr)
+    {
+        foreach ($arr as $var => $val) {
+            $this->render_vars[$var] = $val;
+        }
     }
 
     /**
      * @param $filename
      */
-    protected function set_template($filename){
+    protected function set_template($filename)
+    {
         $this->template = $this->twig->loadTemplate($filename);
+    }
+
+    /**
+     * @return string
+     */
+    public function render()
+    {
+        return $this->template->render($this->render_vars);
     }
 }
