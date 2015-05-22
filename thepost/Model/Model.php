66b06d48626237310693a0ab1f9579544b05697d
speedwhy <lukas.walker@bluewin.ch>
2015-05-22 11:54:54 +0200

    added settings to route

--- thepost/Model/Model.php
+++ thepost/Model/Model.php
@@ -29,6 +29,6 @@ class Model {
         $dsn = 'mysql:dbname=thepost;host=localhost';
         $user = 'lukas';
         $password = 'abcd';
-        $this->pdo = new \PDO($dsn,$user,$password);
+        $this->pdo = new \PDO($dsn,$user,$password, array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
     }
 }
