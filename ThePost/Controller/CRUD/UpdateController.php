73522a07ececaae53ec5fa60d4b7efd07fe87a49
bmeism <news.manuelsworld@gmail.com>
2015-05-28 09:10:19 +0200

    Added security :)

--- ThePost/Controller/CRUD/UpdateController.php
+++ ThePost/Controller/CRUD/UpdateController.php
@@ -10,6 +10,7 @@ namespace ThePost\Controller\CRUD;
 
 use ThePost\Model\Repository\EntryRepository;
 use ThePost\Model\Repository\OptionRepository;
+use ThePost\View\ErrorView;
 
 class UpdateController extends CRUDController
 {
@@ -18,31 +19,39 @@ class UpdateController extends CRUDController
     {
         try {
             //TODO remove wait time
-            if($this->authentication->isLogin()){
-            //var_dump($param);
-            switch ($param['type']){
-                case 'entry':
+            if ($this->authentication->isLogin()) {
+                switch ($param['type']) {
+                    case 'entry':
+                        if (!isset($_POST['title']) & !isset($_POST['text'])) {
+                            throw new \Exception("No content given to update.");
+                        }
+                        $title = $_POST['title'];
+                        $text = $_POST['text'];
+                        $entry_repository = new EntryRepository($this->model->pdo);
+                        $entry_repository->update(intval($param['slug']), $title, $text);
+                        break;
+                    case 'setting':
+                        if (!isset($param['slug']) & !isset($_POST['value'])) {
+                            throw new \Exception("No content given to update.");
+                        }
+                        $key = $param['slug'];
+                        $value = $_POST['value'];
+                        $options_repository = new OptionRepository($this->model->pdo);
+                        $options_repository->update($key, $value);
+                        break;
+                }
+                sleep(1);
 
-                    $title = $_POST['title'];
-                    $text = $_POST['text'];
-                    $entry_repository = new EntryRepository($this->model->pdo);
-                    $entry_repository->update(intval($param['slug']), $title, $text);
-                    break;
-                case 'setting':
-                    $key = $param['slug'];
-                    $value = $_POST['value'];
-                    $options_repository = new OptionRepository($this->model->pdo);
-                    $options_repository->update($key, $value);
-                    break;
-            }
-            sleep(1);
-
-            }else{
-                throw new \Exception("You are not logged in.");
+            } else {
+                throw new \Exception("You are not allowed to update, because you are not logged in.");
             }
 
         } catch (\Exception $e) {
             header("HTTP/1.0 404 Not Found");
+            $this->view = new ErrorView('Error: ', '', $e->getMessage());
+            $this->get_options();
+            $this->set_vars();
+            echo $this->view->render();
         }
     }
 }
