4501b6b9fd4bba7f389b9686bdcea82dc1866d1b
bmeism <news.manuelsworld@gmail.com>
2015-05-29 15:15:04 +0200

    Added config file

--- ThePost/Model/Model.php
+++ ThePost/Model/Model.php
@@ -8,12 +8,14 @@
 
 namespace ThePost\Model;
 
+use ThePost\Controller\Exception\ConfigException;
 
 /**
  * Class Model
  * @package ThePost\Model
  */
-class Model {
+class Model
+{
 
     /**
      * FIY: PDO is a standardized wrapper for any database
@@ -26,9 +28,32 @@ class Model {
      */
     function __construct()
     {
-        $dsn = 'mysql:dbname=thepost;host=localhost';
-        $user = 'lukas';
-        $password = 'abcd';
-        $this->pdo = new \PDO($dsn,$user,$password, array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        if (file_exists('config.json')) {
+
+
+            try {
+                $config = json_decode(file_get_contents('config.json'));
+                if (is_a($config, 'stdClass')) {
+                    foreach ($config as $key => $value) {
+                        if (empty($value)) {
+                            throw new \Exception("Database Config: $key not set");
+                        }
+                    }
+                } else {
+                    throw new \Exception("Database Config is not valid");
+                }
+
+                $dsn = "mysql:dbname={$config->name};host={$config->location};port={$config->port}";
+                $user = $config->username;
+                $password = $config->password;
+
+                $this->pdo = new \PDO($dsn, $user, $password, array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+            } catch (\Exception $e) {
+                throw new ConfigException($e->getMessage() . " <a class='btn btn-primary' href='/install/'>Configure Databse</a>");
+            }
+
+        } else {
+            throw new ConfigException("Your database isn't yet configured! <a href='/install/'>Configure</a> it now.");
+        }
     }
-}
+}+
