22771e3590af4d28fdf2330f0be33a9a3c77b32d
bmeism <news.manuelsworld@gmail.com>
2015-05-29 15:03:45 +0200

    Added Config exception

---
+++ ThePost/Controller/Exception/ConfigException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bmeism
+ * Date: 29.05.2015
+ * Time: 09:53
+ */
+
+namespace ThePost\Controller\Exception;
+
+
+class ConfigException extends \Exception{
+
+}
