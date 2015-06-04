68b2a39bd1e5e42879934d14babff40dc040ddb9
bmeism <news.manuelsworld@gmail.com>
2015-06-04 10:59:45 +0200

    Added PHPDocs

--- ThePost/View/EntryView.php
+++ ThePost/View/EntryView.php
@@ -3,28 +3,33 @@
 namespace ThePost\View;
 
 
+/**
+ * Class EntryView
+ * @package ThePost\View
+ */
 class EntryView extends View{
 
+    /**
+     * Calls parent
+     */
     function __construct()
     {
         parent::__construct();
-
-
     }
 
+    /**
+     * Displays entry.twig
+     */
     public function create(){
-
-
-
         $this->set_template("entry.twig");
-
     }
 
+    /**
+     * Displays entry.twig and show the entry
+     * @param $entry
+     */
     public function read($entry){
-
         $this->render_vars['entry'] = $entry;
-
         $this->set_template("entry.twig");
-
     }
 }
