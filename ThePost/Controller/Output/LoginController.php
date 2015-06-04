c92c4f2f38227d40128129144cd8aece51e8a114
bmeism <news.manuelsworld@gmail.com>
2015-06-04 14:56:49 +0200

    Throws exception if no POST is given

--- ThePost/Controller/Output/LoginController.php
+++ ThePost/Controller/Output/LoginController.php
@@ -10,13 +10,24 @@ namespace ThePost\Controller\Output;
 
 use ThePost\View\LoginView;
 
+/**
+ * Class LoginController
+ * @package ThePost\Controller\Output
+ */
 class LoginController extends MainController {
 
+    /**
+     * Displays login
+     */
     public function login(){
         $this->view = new LoginView();
         $this->view->add_render_vars(array("site"=>array("login"=>true)));
     }
 
+    /**
+     * Gets login per POST
+     * @throws \Exception
+     */
     public function submit(){
         if(isset($_POST['login'])){
             $username = trim($_POST['username']);
@@ -39,9 +50,15 @@ class LoginController extends MainController {
                 $this->view->add_render_vars(array('auth'=>array('failed'=>true)));
             }
 
+        }else {
+            header("HTTP/1.0 406 No content given via POST",true,406);
+            throw new \Exception("No login details given via <a href='/login/'>login</a>.");
         }
     }
 
+    /**
+     * Loggs the user out and unsets the SESSION
+     */
     public function logout()
     {
         unset($_SESSION['user_id']);
