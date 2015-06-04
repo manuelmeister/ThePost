55d7b68f231c897805db19fc4be7ca63f4b7b7ee
bmeism <news.manuelsworld@gmail.com>
2015-06-04 11:39:58 +0200

    Added PHPDocs Extracted methods from MainController

--- ThePost/Controller/BasicController.php
+++ ThePost/Controller/BasicController.php
@@ -1,15 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bmeism
- * Date: 29.05.2015
- * Time: 09:28
- */
 
 namespace ThePost\Controller;
 
 
+use ThePost\Model\Entity\Options;
 use ThePost\Model\Entity\User;
+use ThePost\Model\Model;
+use ThePost\Model\Repository\OptionRepository;
 use ThePost\View\View;
 
 /**
@@ -24,12 +21,50 @@ class BasicController {
     public $view;
 
     /**
+     * @var Model
+     */
+    public $model;
+
+    /**
+     * @var array
+     */
+    protected $options_array = array();
+
+    /**
+     * Gets all Settings(Options) from Repository and fills it in an associative array like:
+     *
+     *      array(
+     *          key1 => value1
+     *          key2 => value2
+     *      )
+     *
+     */
+    public function get_options()
+    {
+        $options_repository = new OptionRepository($this->model->pdo);
+        $options = $options_repository->findAll();
+
+        foreach ($options as $option) {
+            /** @var Options $option */
+            $this->options_array[$option->getKey()] = $option->getValue();
+        }
+    }
+
+    /**
+     * Set render vars
+     */
+    public function view_set_vars()
+    {
+        $this->view->set_render_vars($this->options_array,$this->authentication);
+    }
+
+    /**
      * @var User
      */
     protected $authentication;
 
     /**
-     * @param $authentication
+     * @param $authentication User
      */
     public function set_authentication($authentication)
     {
